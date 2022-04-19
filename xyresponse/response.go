f0135b176d5c4845bfa13481c9336832b7387087
sangvhh21 <sangvhh21@gmail.com>
2022-04-19 17:28:58 +0700

    fix bug

--- xyresponse/response.go
+++ xyresponse/response.go
@@ -1,42 +1,33 @@
 package xyresponse
 
-import (
-	"fmt"
-)
-
-type metadata struct {
-	service string
-	version float64
-	errno int
-	errer string
-}
-
 type xyresponse struct {
-	data string
-	meta metadata
+	data any
+	meta map[string]any
 }
 
 func NewGenerator(serv string, ver float64) xyresponse {
 	return xyresponse{
 		data: "",
-		meta: {
-			service: serv,
-			version: ver,
-			errno: 0,
+		meta: map[string]any{
+			"service": serv,
+			"version": ver,
+			"errno":   0,
+			"error":   "",
 		},
 	}
 }
 
-func (x *xyresponse) New(new_dt string) xyresponse {
+func (x *xyresponse) NewData(new_dt any) xyresponse {
 	x.data = new_dt
+	return *x
 }
 
-func (x *xyresponse) NewError(new_err string) xyresponse {
-	x.errer = new_err
+func (x *xyresponse) NewError(new_err error) xyresponse {
+	x.meta["error"] = new_err
+	return *x
 }
 
-func (x *xyresponse) New(new_dt string, new_mt metadata){
-	x.data = new_dt,
-	x.meta = new_mt
+func (x *xyresponse) NewMeta(new_key string, new_value any) {
+	x.meta[new_key] = new_value
+	return
 }
-
