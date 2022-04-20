d5072753630839e765a86469deda480080aa94a7
sangvhh21 <sangvhh21@gmail.com>
2022-04-20 11:19:19 +0700

    add xyrespone_test

--- xyresponse/example_test.go
+++ xyresponse/example_test.go
@@ -1,1 +1,13 @@
 package xyresponse_test
+
+import (
+	"github.com/xybor/xyplatform/xyresponse"
+)
+
+var ExampleResponse = xyresponse.NewGenerator("XyAuth", 1)
+
+func ExampleNewData() {
+	new_data := "new data"
+	ExampleResponse.NewData(new_data)
+	return
+}
