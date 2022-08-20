fce1c434b78cb1639a22e2a1ec4463b3c9a98f3f
huykingsofm <huykingsofm@gmail.com>
2022-08-20 15:45:10 +0000

    PLF-63 Write unittest and benchmark of xyerror

---
+++ xyerror/class_test.go
@@ -0,0 +1,30 @@
+package xyerror_test
+
+import (
+	"testing"
+
+	"github.com/xybor/xyplatform/xycond"
+	"github.com/xybor/xyplatform/xyerror"
+)
+
+func TestClassNewClass(t *testing.T) {
+	var id = nextid()
+	var egen = xyerror.Register("gen", id)
+	var c1 = egen.NewClass("class1")
+	var c2 = c1.NewClass("class2")
+	xycond.MustEqual(c1.Error(), classmsg(id+1, "class1")).
+		Testf(t, "%s != %s", c1.Error(), classmsg(id+1, "class1"))
+	xycond.MustEqual(c2.Error(), classmsg(id+2, "class2")).
+		Testf(t, "%s != %s", c2.Error(), classmsg(id+2, "class2"))
+}
+
+func TestClassNewClassM(t *testing.T) {
+	var id1 = nextid()
+	var id2 = nextid()
+	var egen1 = xyerror.Register("gen", id1)
+	var egen2 = xyerror.Register("gen", id2)
+	var c1 = egen1.NewClass("class")
+	var c2 = c1.NewClassM(egen2)
+	xycond.MustEqual(c2.Error(), classmsg(id2+1, "class")).
+		Testf(t, "%s != %s", c2.Error(), classmsg(id2+1, "class"))
+}
---
+++ xyerror/error_test.go
@@ -0,0 +1,64 @@
+package xyerror_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/xybor/xyplatform/xycond"
+	"github.com/xybor/xyplatform/xyerror"
+)
+
+func TestXyError(t *testing.T) {
+	var id = nextid()
+	var egen = xyerror.Register("", id)
+	var c = egen.NewClass("class")
+	var xerr = c.New("error")
+	xycond.MustEqual(xerr.Error(), "class: error").
+		Testf(t, "%s != %s", xerr.Error(), "class: error")
+}
+
+func TestXyErrorIs(t *testing.T) {
+	var err1 = xyerror.ValueError.New("err1")
+	var err2 = xyerror.TypeError.New("err2")
+
+	xycond.MustTrue(errors.Is(err1, xyerror.ValueError)).
+		Testf(t, "err1 should be xyerror.ValueError")
+	xycond.MustTrue(errors.Is(err2, xyerror.TypeError)).
+		Testf(t, "err2 should be xyerror.TypeError")
+
+	xycond.MustFalse(errors.Is(err1, err1)).
+		Testf(t, "err1 should not be err1")
+	xycond.MustFalse(errors.Is(err1, err2)).
+		Testf(t, "err1 should not be err2")
+	xycond.MustFalse(errors.Is(err1, xyerror.TypeError)).
+		Testf(t, "err1 should not be xyerror.TypeError")
+}
+
+func TestOr(t *testing.T) {
+	var err1 = xyerror.ValueError.New("err1")
+	var err2 = xyerror.TypeError.New("err2")
+	var err3 error = nil
+
+	xycond.MustTrue(errors.Is(xyerror.Or(err1, err2), xyerror.ValueError)).
+		Testf(t, "err1 or err2 should be the ValueError")
+	xycond.MustTrue(errors.Is(xyerror.Or(err2, err1), xyerror.TypeError)).
+		Testf(t, "err2 or err1 should be the TypeError")
+	xycond.MustNil(xyerror.Or(nil, err3)).
+		Testf(t, "Or with all nil errors should return nil")
+}
+
+func TestCombine(t *testing.T) {
+	var gen = xyerror.Register("", nextid())
+	var c = xyerror.Combine(xyerror.ValueError, xyerror.TypeError).
+		NewClass(gen, "class")
+	var xerr = c.New("error")
+
+	xycond.MustTrue(errors.Is(xerr, xyerror.ValueError)).
+		Testf(t, "xerr should be ValueError")
+
+	xycond.MustTrue(errors.Is(xerr, xyerror.TypeError)).
+		Testf(t, "xerr should be TypeError")
+
+	xycond.MustFalse(errors.Is(xerr, xyerror.IndexError)).
+		Testf(t, "xerr should not be IndexError")
+}
