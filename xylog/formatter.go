54bfaf28b33f2cfbfe8302a80819d57d3a38edda
huykingsofm <huykingsofm@gmail.com>
2022-08-24 17:21:39 +0000

    PLF-58 Improve style in format log message

--- xylog/formatter.go
+++ xylog/formatter.go
@@ -2,8 +2,8 @@ package xylog
 
 import (
 	"fmt"
-	"reflect"
-	"strings"
+
+	"github.com/xybor/xyplatform/xycond"
 )
 
 // Formatter instances are used to convert a LogRecord to text.
@@ -18,46 +18,56 @@ type Formatter interface {
 // The TextFormatter can be initialized with a format string which makes use of
 // knowledge of the LogRecord attributes - e.g. %(message)s or %(levelno)d. See
 // LogRecord for more details.
-type textFormatter string
+type textFormatter struct {
+	formatstring  string
+	attrbuteIndex []int
+}
 
 // NewTextFormatter creates a textFormatter which uses LogRecord attributes to
 // contribute logging string, e.g. %(message)s or %(levelno)d. See LogRecord for
 // more details.
-func NewTextFormatter(f string) textFormatter {
-	return textFormatter(f)
+func NewTextFormatter(s string) textFormatter {
+	var record = LogRecord{}
+	var attributeIndex []int
+	var fmtstr = ""
+	var i, n = 0, len(s)
+	for i < n {
+		fmtstr += string(s[i])
+		if s[i] == '%' {
+			xycond.MustTrue(i+1 < n).Assert("unexpectedly end with %%")
+			i++
+			switch s[i] {
+			case '%':
+			case '(':
+				i++
+				var token = ""
+				for {
+					xycond.MustTrue(i < n).Assert("uncompleted token %s", token)
+					if s[i] == ')' {
+						break
+					}
+					token += string(s[i])
+					i++
+				}
+				attributeIndex = append(attributeIndex, record.mapName(token))
+			default:
+				xycond.Panic("unexpected token: %s", s[i-2:i])
+			}
+		}
+		i++
+	}
+
+	return textFormatter{
+		formatstring:  fmtstr,
+		attrbuteIndex: attributeIndex,
+	}
 }
 
 // Format creates a logging string by combine format string and logging record.
 func (f textFormatter) Format(record LogRecord) string {
-	var m = tomap(record)
-	var s = string(f)
-	for k, v := range m {
-		var token = "%(" + k + ")" // %(foo)
-		for {
-			var index = strings.Index(s, token)
-			if index == -1 {
-				break
-			}
-			token = s[index : index+len(token)+1]    // %(foo)s
-			var verb = "%" + token[len(token)-1:]    // %s
-			var value = fmt.Sprintf(verb, v)         // bar
-			s = strings.Replace(s, token, value, -1) // %(foo)s -> bar
-		}
+	var attrs = make([]any, len(f.attrbuteIndex))
+	for i := range f.attrbuteIndex {
+		attrs[i] = record.mapIndex(f.attrbuteIndex[i])
 	}
-
-	return s
-}
-
-// tomap converts a struct to map[string]any.
-func tomap(a any) map[string]any {
-	var result = make(map[string]any)
-	var v = reflect.ValueOf(a)
-	for i := 0; i < v.NumField(); i++ {
-		var f = v.Type().Field(i)
-		var tag = f.Tag.Get("map")
-		if tag != "" {
-			result[tag] = v.Field(i).Interface()
-		}
-	}
-	return result
+	return fmt.Sprintf(f.formatstring, attrs...)
 }
